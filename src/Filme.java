562fdcb0e808dc091f14a07ba7ded49f7f46e41f
FelipeDev2020 <luisfelipexv20@gmail.com>
2025-07-31 11:40:07 -0300

    Novo Commit

--- src/Filme.java
+++ src/Filme.java
@@ -1,6 +1,22 @@
 public class Filme {
     String nome;
+    int anodelancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDaAvaliacao;
     int totalDeAvaliacao;
+    int duracaoEmMinutos;
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome: " + nome + " | Ano de lançamento: " + anodelancamento + " | Nota do filme: " + pegaMedia());
+    }
+
+    public void avalia(double nota){
+        somaDaAvaliacao += nota;
+        totalDeAvaliacao++;
+    }
+
+    public String pegaMedia(){
+        return String.format("%.1f", somaDaAvaliacao / totalDeAvaliacao);
+    }
+
 }
